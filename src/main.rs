a12b9daaa823f32acc562f7f44854542e2b61429
Igor Semyonov <igor@semyonov.xyz>
2024-07-08 22:05:34 -0400

    changed bacgorund color, removed stepping, and added some stuff to speed up sompiles

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 //! A simplified implementation of the classic game "Breakout".
 //!
-//! Demonstrates Bevy's stepping capabilities if compiled with the `bevy_debug_stepping` feature.
 
 use bevy::{
     math::bounding::{Aabb2d, BoundingCircle, BoundingVolume, IntersectsVolume},
@@ -8,13 +7,12 @@ use bevy::{
     sprite::MaterialMesh2dBundle,
 };
 
-mod stepping;
 
 // These constants are defined in `Transform` units.
 // Using the default 2D camera they correspond 1:1 with screen pixels.
 const PADDLE_SIZE: Vec2 = Vec2::new(120.0, 20.0);
 const GAP_BETWEEN_PADDLE_AND_FLOOR: f32 = 60.0;
-const PADDLE_SPEED: f32 = 500.0;
+const PADDLE_SPEED: f32 = 330.0;
 // How close can the paddle get to the wall
 const PADDLE_PADDING: f32 = 10.0;
 
@@ -22,7 +20,7 @@ const PADDLE_PADDING: f32 = 10.0;
 const BALL_STARTING_POSITION: Vec3 = Vec3::new(0.0, -50.0, 1.0);
 const BALL_DIAMETER: f32 = 30.;
 const BALL_SPEED: f32 = 400.0;
-const INITIAL_BALL_DIRECTION: Vec2 = Vec2::new(0.5, -0.0);
+const INITIAL_BALL_DIRECTION: Vec2 = Vec2::new(0.5, -0.5);
 
 const WALL_THICKNESS: f32 = 10.0;
 // x coordinates
@@ -43,7 +41,7 @@ const GAP_BETWEEN_BRICKS_AND_SIDES: f32 = 20.0;
 const SCOREBOARD_FONT_SIZE: f32 = 40.0;
 const SCOREBOARD_TEXT_PADDING: Val = Val::Px(5.0);
 
-const BACKGROUND_COLOR: Color = Color::srgb(0.9, 0.9, 0.9);
+const BACKGROUND_COLOR: Color = Color::srgb(0.0, 0.0, 0.0);
 const PADDLE_COLOR: Color = Color::srgb(0.3, 0.3, 0.7);
 const BALL_COLOR: Color = Color::srgb(1.0, 0.5, 0.5);
 const BRICK_COLOR: Color = Color::srgb(0.5, 0.5, 1.0);
@@ -52,14 +50,8 @@ const TEXT_COLOR: Color = Color::srgb(0.5, 0.5, 1.0);
 const SCORE_COLOR: Color = Color::srgb(1.0, 0.5, 0.5);
 
 fn main() {
-    App::new()
+    let run = App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(
-            stepping::SteppingPlugin::default()
-                .add_schedule(Update)
-                .add_schedule(FixedUpdate)
-                .at(Val::Percent(35.0), Val::Percent(50.0)),
-        )
         .insert_resource(Score(0))
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .add_event::<CollisionEvent>()
